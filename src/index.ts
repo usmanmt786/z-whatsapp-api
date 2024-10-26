b2840dbd46cb5485c8f4eb18c4a55d18a8885a57
fathah-work <abdulfathahkodag@gmail.com>
2024-10-26 12:13:07 +0400

    Send Catches

--- src/index.ts
+++ src/index.ts
@@ -65,9 +65,13 @@ app.post('/:num/message', apiAuthorize, async (req, res) => {
             return res.status(400).json({ success: false, message: 'Invalid phone number' });
         }
 
-        await waclient.sendMessage(`${finalNumber}@c.us`, message);
+        try {
+        const resp =     await waclient.sendMessage(`${finalNumber}@c.us`, message);
 
-        return res.status(200).json({ success: true, message: 'Message sent successfully' });
+        return res.status(200).json({ success: true, message: 'Message sent successfully',resp:`${resp}` });
+        } catch (error) {
+            return res.status(500).json({ success: true, message: 'Failed to send message',error:`${error}` });
+        }
 
     } catch (error) {
         console.error('Error sending message:', error);
